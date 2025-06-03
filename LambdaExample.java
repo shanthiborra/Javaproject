538cf1aaaaefeca0a7e169df82af8af04d975cfc
shanthiborra <borrashanthi22@gmail.com>
2025-06-03 14:17:27 +0530

    Add files via upload

---
+++ LambdaExample.java
@@ -0,0 +1,12 @@
+package xyz;
+
+ interface message{
+	 void say();
+ }
+ public class LambdaExample{
+	 
+	 public static void main(String[] args) {
+		 message hellomessage=()-> System.out.println("This is Lambda ");
+		 hellomessage.say();
+	 }
+ }
