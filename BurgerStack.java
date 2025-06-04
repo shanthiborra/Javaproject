ed3a67ef66ac2e2fad2ad7b298947346c205bbb7
shanthiborra <borrashanthi22@gmail.com>
2025-06-04 14:58:47 +0530

    Add files via upload

---
+++ BurgerStack.java
@@ -0,0 +1,18 @@
+package xyz;
+import java.util.*;
+public class BurgerStack {
+
+	public static void main(String[] args) {
+		Stack<String> burgerStack = new Stack<String>();
+		 burgerStack.push("Burger 1");
+		 burgerStack.push("Burger 2");
+		 burgerStack.push("Burger 3");
+		 System.out.println("Top burger is" + burgerStack.peek());
+		 System.out.println("Serving" + burgerStack.pop());
+		 if(burgerStack.isEmpty()) {
+			 System.out.println("All burger are served");
+		 }
+		  
+	}
+
+}
